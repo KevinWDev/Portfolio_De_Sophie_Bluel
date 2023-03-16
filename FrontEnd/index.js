4728a5c00969b243a13f0970fbf1b14527a1be13
Kevin W <kevinwatteau@outlook.fr>
2023-03-16 21:49:41 +0100

    Génération des filtres, modal bien avancé, génération des <option> catégories

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,29 +1,114 @@
+let travaux = [];
 
-// // Récupération de l'API
-const reponse = await fetch(`http://localhost:5678/api/works/`, {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-}
-);
-// Réponse de l'API au format JSON
-const travaux = await reponse.json();
-console.log(travaux)
+export async function getWorks() {  
+    // Appel de l'API
+    const reponse = await fetch(`http://localhost:5678/api/works/`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+    });
+    if (reponse.ok === true) {
+        // Réponse de l'API
+        travaux = await reponse.json();
+        console.log(travaux)
+        
+        genererElements(travaux)
+        
+    } else {
+        throw new Error("Impossible d'accéder au serveur !");
+    };
+};
 
-// Création des bouton en JS
-let sectionPortfolio = document.querySelector('#portfolio')
-    .innerHTML = '<h2>Mes Projets</h2>' +
-    '<form id="filters">'
-    + '<button id="buttonAll" class="btn" >Tous</button>'
-    + '<button id="buttonObjects" class="btn" >Objets</button>'
-    + '<button id="buttonAppartments" class="btn" >Appartements</button>'
-    + '<button id="buttonHotelAndRestaurants" class="btn" >Hôtels & restaurants</button>' +
-    '</form>'
-    + '<div class="gallery"></div>';
+await getWorks();
 
-// Boucle pour parcourir les éléments du tableau works
-function genererElements(travaux) {
+
+// ----------- GENERER BOUTON AVEC API CATEGORIE --------------
+
+// Récupération des catégoriees depuis l'API
+let categories;
+
+async function getCategory() {
+    const reponse = await fetch(`http://localhost:5678/api/categories`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            "Content-Type": "application/json"
+        },
+    });
+
+    if (reponse.ok === true) {
+        // Réponse de l'API (catégories) au format JSON
+        categories = await reponse.json();
+        console.log(categories);
+
+        genererBoutons(travaux);
+        
+
+    } else {
+        throw new Error("Impossible d'accéder au serveur !");
+    };
+};
+
+getCategory();
+
+let btn;
+
+const sectionPortfolio = document.querySelector('#portfolio');
+const formFilters = document.createElement('form');
+formFilters.setAttribute("id", "filters");
+
+sectionPortfolio.appendChild(formFilters);
+
+const gallery = document.querySelector('.gallery');
+formFilters.after(gallery);
+
+async function genererBoutons() {
+    let boutonAll = {
+        id: 0,
+        name: 'Tous'
+    };
+    categories.unshift(boutonAll);
+
+    for (let categorie of categories) {
+        console.log(categorie.id);
+        btn = document.createElement('button');
+        
+        btn.innerText = categorie.name;
+        btn.setAttribute("id", categorie.id);
+        btn.setAttribute("class", "btn");
+        
+        btn.style.padding = "9px 30px";
+        
+        formFilters.appendChild(btn);
+        btn.addEventListener("click", function (e) {
+            e.preventDefault();
+            const category = travaux.filter(function (all) {
+                // Si catégorie = 0 alors recupère tous sinon récupère que ceux ou l'id match
+                if (categorie.id === 0) {
+                    return true;
+                 } else {
+                    return all.categoryId === categorie.id;
+                 };
+        
+            });
+    
+            console.log(category);
+            
+            document.querySelector('.gallery').innerHTML = "";
+            genererElements(category);
+            
+        });
+    
+        };
+ 
+
+    };
+
+
+// Fonction qui génère les travaux   
+export async function genererElements(travaux) {
     for (let i = 0; i < travaux.length; i++) {
 
         // Création d'une balise dédiée au éléments  du tableau
@@ -38,117 +123,13 @@ function genererElements(travaux) {
         cardElement.appendChild(imageElement);
         cardElement.appendChild(figcaptionElement);
 
-
         // On rattache la balise figure à son parent .gallery
         document.querySelector(".gallery")
             .appendChild(cardElement);
-
-    };
-    
+        
+          let b = travaux[i].id
+          console.log(b)
+          
+          
+    };       
 };
-
-genererElements(travaux);
-
-document.querySelector('#buttonAll')
-    .addEventListener("click", function (e) {
-        e.preventDefault();
-        const allCategory = travaux.filter(function (all) {
-            return all.categoryId
-        });
-
-        console.log(allCategory);
-
-        document.querySelector('.gallery').innerHTML = "";
-        genererElements(allCategory);
-
-    });
-
-document.querySelector('#buttonObjects')
-    .addEventListener("click", function (event) {
-        event.preventDefault();
-        const categoryObjects = travaux.filter(function (objets) {
-            return objets.categoryId === 1;
-        });
-        console.log(categoryObjects)
-
-        document.querySelector('.gallery').innerHTML = "";
-        genererElements(categoryObjects);
-    });
-
-document.querySelector('#buttonAppartments')
-    .addEventListener("click", function (e) {
-        e.preventDefault();
-        const categoryAppartments = travaux.filter(function (appartments) {
-            return appartments.categoryId === 2;
-        });
-        console.log(categoryAppartments);
-
-        document.querySelector(".gallery").innerHTML = "";
-        genererElements(categoryAppartments);
-    });
-
-document.querySelector('#buttonHotelAndRestaurants')
-    .addEventListener("click", function (e) {
-        e.preventDefault();
-        const categoryHotelsAndRestaurants = travaux.filter(function (hotelsAndRestaurants) {
-            return hotelsAndRestaurants.categoryId === 3;
-        });
-        console.log(categoryHotelsAndRestaurants);
-
-        document.querySelector(".gallery").innerHTML = "";
-        genererElements(categoryHotelsAndRestaurants);
-    });
-
-
-// Création des bouton en JS
-/*let sectionPortfolio = document.querySelector('#portfolio')
- .innerHTML =  '<h2>Mes Projets</h2>' +
- '<form id=formFilter>'
- + '<button id=buttonAll class=btn >Tous</button>'
- + '<button id=buttonObjects class=btn >Objets</button>'
- + '<button id=buttonAppartments class=btn >Appartements</button>'
- + '<button id=buttonHotelAndRestaurants class=btn >Hôtels & restaurants</button>'
- '</form>';
- 
- const formFilter = document.querySelector('#formFilter')
- formFilter.style.display = 'flex';
- formFilter.style.justifyContent = 'center';
- formFilter.style.gap = '10px';
- formFilter.style.marginBottom = '51px';
- 
-let allButton = document.querySelectorAll('.btn');
- 
- 
-for (let btn of allButton) {
- btn.style.border = '1px solid #1D6154'
- btn.style.borderRadius = '60px';
- btn.style.height = '37px';
- btn.style.font = 'inherit';
- btn.style.color = '#1D6154';
- btn.style.background = 'white';
- btn.style.fontWeight = '700';
- btn.style.fontFamily = 'syne';
- btn.addEventListener("mouseenter", function(e) {
-     
-     btn.style.background = '#1D6154';
-     btn.style.color = 'white';
-     
- })
- 
- btn.addEventListener("mouseleave", function (e) {
-     btn.style.background = 'white';
-     btn.style.color = '#1D6154'
- 
- })
- 
-}
- 
-document.querySelector('#buttonAll')
- .style.padding = '9px 30px 9px 28px';
-document.querySelector('#buttonObjects')
- .style.padding = '9px 22px 9px 20px';
-document.querySelector('#buttonAppartments')
- .style.padding = '9px 15px 9px 14px';
-document.querySelector('#buttonHotelAndRestaurants')
- .style.padding = '9px 11px 9px 9px';
-*/ 
--- FrontEnd/logIn.js
+++ FrontEnd/logIn.js
@@ -1,30 +1,213 @@
-const user = {
-    email: document.querySelector('email').value,
-    password: document.querySelector('password').value
-  };
-  console.log(user)
+// Athentification
+async function connexion() {
 
+  const formulaireLogIn = document.querySelector('#formLogIn');
 
-  let response = await fetch('http://localhost:5678/api/users/login', {
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(user)
-  });
-  
-  let result = await response.json();
-  console.log(result)
-
-
-
-const formLogIn = document.querySelector('#formLogIn')
-
-
-formLogIn.addEventListener("input", async function(event) {
+  formulaireLogIn.addEventListener("submit", async function (event) {
     event.preventDefault()
 
-    
-})
+    let users = {
+      email: document.querySelector('#email').value,
+      password: document.querySelector('#password').value
+    };
 
+
+    console.log(users);
+  //  Envoie à l'API
+    const response = await fetch(`http://localhost:5678/api/users/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+
+      body: JSON.stringify(users)
+
+    });
+
+    if (response.ok === true) {
+      // Récupération de la réponse
+      console.log(response);
+      const result = await response.json();
+      console.log(result);
+
+      // On stocke le token
+      sessionStorage.setItem("token", result.token);
+      const token = sessionStorage.getItem("token");
+      window.location.href = 'index.html';
+      
+    } else {
+      // On informe l'utilisateur si email ou MDP incorrect
+      let myError = document.querySelector('#error');
+      myError.innerHTML = "Identifiant ou mot de passe incorrecte !";
+      myError.style.color = "red";
+      
+    } 
+     
+    throw new Error("Impossible d'accéder au serveur !");
+  
+  });
+};
+
+connexion();
+
+
+
+// // HEADER AVEC BANNIERE EDITEUR
+// if (sessionStorage.getItem("token")) {
+//   // Récupération de la balise header
+//   const headerIndex = document.querySelector('header');
+//   headerIndex.setAttribute("id", "headerEdition")
+//   headerIndex.style.margin = "0";
+  
+//   // Création d'une div pour la bannière en mode editeur
+//   const banniereDiv = document.createElement('div');
+//   banniereDiv.setAttribute("id", "bloc-mode-editeur")
+
+//   // Récupération du titre et du menu de nav
+//   const titreIndex = document.querySelector('h1');
+//   const navIndex = document.querySelector('header nav');
+  
+//   // Création d'une div blocHeader
+//   const blocHeader = document.createElement('div');
+//   blocHeader.setAttribute("id", "bloc-header")
+
+//   // Ajout des enfants (titre et menu nav) au blocHeader
+//   blocHeader.append(titreIndex);
+//   blocHeader.append(navIndex)
+
+//   // Ajout des enfants (div : blocHeader et bannière) au header
+//   headerIndex.append(blocHeader)
+//   headerIndex.append(banniereDiv)
+
+//   // Placement de la bannière avant le blocHeader
+//   banniereDiv.after(blocHeader)
+  
+//   // Ajout de l'icone, paragraphe et bouton
+//   banniereDiv.innerHTML = `<i class="fa-regular fa-pen-to-square"></i>`
+//     +`<p>Mode édition</p>`
+//     +`<button type="submit">publier les changements</button>`;
+
+// };
+
+
+// // BOUTON MODIFIER
+// if (sessionStorage.getItem("token")) {
+//   // BOUTON MODIFIER INTRODUCTION
+//   // Récupération de la balise figure
+//   const figureIntroIndex = document.querySelector('#introduction figure');
+//   figureIntroIndex.setAttribute("id", "figureIntro")
+  
+//   // Création du bouton modifier
+//   const btnModifier = document.createElement('a')
+//   btnModifier.setAttribute("id", "btnModifierFigure")
+//   btnModifier.innerHTML = `<i class="fa-regular fa-pen-to-square"></i>modifier`;
+  
+//   // Ajout du bouton au parent
+//   figureIntroIndex.append(btnModifier)
+
+
+//   // BOUTON MODIFIER MES PROJETS H2
+//   // Récupération de la section avec id portfolio et du titre h2
+//   const portfolio = document.querySelector('#portfolio');
+//   const titleProjets = document.querySelector('#portfolio h2');
+
+//   // Création d'une div
+//   const divH2 = document.createElement('div')
+//   divH2.setAttribute("id", "bloc-h2")
+
+//   // Récupération des filtres pour display none en mode édition
+//   const filters = document.querySelector('#filters')
+//   filters.style.display = "none"
+
+  
+//   // Ajout du h2 à la div
+//   divH2.append(titleProjets)
+
+//   // Ajout de la div à la section portfolio
+//   portfolio.append(divH2)
+
+//   // Passer la divH2 avant les filtres
+//   filters.before(divH2)
+//   // divH2.after(filters)
+
+//   // Création du bouton modifier
+//   const btnModifierH2 = document.createElement('a');
+//   btnModifierH2.setAttribute("id", "btnModifierH2");
+//   btnModifierH2.innerHTML = `<i class="fa-regular fa-pen-to-square"></i>modifier`;
+
+//   // Ajout du bouton à la divH2
+//   divH2.append(btnModifierH2)
+  
+
+
+  
+//   // const btnModifierH2 = document.createElement('a')
+//   // btnModifierH2.setAttribute("id", "btnModifierH2");
+//   // btnModifierH2.innerHTML = `<i class="fa-regular fa-pen-to-square"></i>modifier`;
+
+//   // titleProjets.append(btnModifierH2)
+
+  
+
+// }
+
+
+
+// let inputEmail = document.querySelector('#email');
+// let inputPassword = document.querySelector('#password');
+// let myErrorEmail = document.querySelector('#errorEmail');
+// let myErrorPassword = document.querySelector('#errorPassword');
+
+
+// async function connexion() {
+
+//   const formulaireLogIn = document.querySelector('#formLogIn');
+
+//   formulaireLogIn.addEventListener("submit", async function (event) {
+//     event.preventDefault()
+
+//     let users = {
+//       email: document.querySelector('#email').value,
+//       password: document.querySelector('#password').value
+//     };
+
+    
+//     console.log(users)
+   
+//     const response = await fetch(`http://localhost:5678/api/users/login`, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+
+//       body: JSON.stringify(users)
+
+//     });
+    
+    
+//     if (response.ok) {
+//       console.log(response)
+//       let result = await response.json()
+//       console.log(result)
+      
+
+//          sessionStorage.setItem("token", result.token);
+      
+//       window.location.href = 'index.html';
+
+//     } 
+//      else {
+
+//    let myError = document.querySelector('#error')
+//       myError.innerHTML = "Identifiant ou mot de passe incorrecte !";
+//       myError.style.color = "red";
+//     } 
+
+//      throw new Error("Impossible d'accéder au serveur !");
+      
+
+
+//   })
+// }
+
+// connexion()
